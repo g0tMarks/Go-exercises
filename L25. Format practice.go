329af232764b4acc48eff40dc84eb9a7d18558b3
Richard Marks <37822503+g0tMarks@users.noreply.github.com>
2025-09-08 22:58:15 +1000

    Continue practice with variables in Go

---
+++ L25. Format practice.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fname := "Dalinar"
+	lname := "Kholin"
+	age := 45
+	messageRate := 0.5
+	isSubscribed := false
+	message := "Sometimes a hypocrite is nothing more than a man in the process of changing."
+
+	// Don't touch above this line
+
+	userLog := fmt.Sprint("Name: FNAME LNAME, Age: AGE, Rate: MESSAGERATE, Is Subscribed: ISSUBSCRIBED, Message: MESSAGE")
+	fmt.Sprintf("Hi %s, your open rate is %.1f percent \n", name, openRate)
+
+	// Don't touch below this line
+
+	fmt.Println(userLog)
+}
