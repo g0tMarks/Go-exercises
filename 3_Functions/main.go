9427ff0b447ca967b0fb4e88c6fa9c54260d9752
Richard Marks <richard.a.marks@gmail.com>
2025-09-17 20:50:47 +1000

    passing variables by value

---
+++ 3_Functions/main.go
@@ -0,0 +1,14 @@
+package main
+
+func getBillForMonth(costPerSend, messageSent int) int {
+	bill := costPerSend * messageSent
+	return bill
+}
+
+func monthlyBillIncrease(costPerSend, numLastMonth, numThisMonth int) int {
+	var lastMonthBill int
+	var thisMonthBill int
+	lastMonthBill = getBillForMonth(costPerSend, numLastMonth)
+	thisMonthBill = getBillForMonth(costPerSend, numThisMonth)
+	return thisMonthBill - lastMonthBill
+}
